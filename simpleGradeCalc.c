e6ff4517f4768859542ab46ea84f9cf1db99b309
Dhruvi Akabari <dhruvipatel3096@gmail.com>
2026-06-04 09:02:02 +0530

    Add files via upload

---
+++ simpleGradeCalc.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main() {
+    int mark;
+
+    printf("Enter your mark: ");
+    scanf("%d", &mark);
+
+    char grade = (mark >= 90) ? 'A' :
+                 (mark >= 70) ? 'B' :
+                 (mark >= 50) ? 'C' :
+                 (mark >= 35) ? 'D' :
+                                'F';
+
+    printf("Your grade is: %c.", grade);
+
+    switch(grade){
+        case 'A':
+            printf(" Excellent work!");
+            break;
+        case 'B':
+            printf(" well done!");
+            break;
+        case 'C':
+            printf(" Good job!");
+            break;
+        case 'F':
+            printf(" Sorry, you failed.");
+            break;
+        default:
+            printf(" invalid grade.");
+        
+    }
+
+    if(grade == 'A' || grade == 'B' || grade == 'C' || grade == 'D') {
+        printf(" you are eligible for the next level.");
+    }
+    else{
+        printf("Please try again next time");
+    }
+}
